c05821c578a3fcdedb03a91539b53326bf3d73f7
lev0607 <lev36rus@gmail.com>
2020-02-18 20:30:59 +0300

    fix README.md, Makefile, .travis.yml, composer.json

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [![Build Status](https://travis-ci.org/lev0607/php-project-lvl2.svg?branch=master)](https://travis-ci.org/lev0607/php-project-lvl2)
 [![Code Climate](https://codeclimate.com/github/lev0607/php-project-lvl2/badges/gpa.svg)](https://codeclimate.com/github/lev0607/php-project-lvl2)
 [![Issue Count](https://codeclimate.com/github/lev0607/php-project-lvl2/badges/issue_count.svg)](https://codeclimate.com/github/lev0607/php-project-lvl2)
+[![Test Coverage](https://codeclimate.com/github/lev0607/php-project-lvl2/badges/coverage.svg)](https://codeclimate.com/github/lev0607/php-project-lvl2/coverage)
--- src/genDiff.php
+++ src/genDiff.php
@@ -4,24 +4,25 @@ namespace Differ\genDiff;
 
 function genDiff($pathToFile1, $pathToFile2)
 {
-	$jsonBefore = json_decode(file_get_contents("$pathToFile1"), true);
-	$jsonAfter = json_decode(file_get_contents("$pathToFile2"), true);
-	$result = ["{"];
-	foreach ($jsonBefore as $key => $value) {
-		if (array_key_exists($key, $jsonAfter)) {
-			$result[] = $jsonAfter[$key] === $jsonBefore[$key] ? "    {$key}: {$jsonBefore[$key]}" : "  - {$key}: {$jsonBefore[$key]}\n  + {$key}: {$jsonAfter[$key]}";
-		} else {
-			$result[] = "  - {$key}: {$jsonBefore[$key]}";
-		}
-	}
-	foreach ($jsonAfter as $key => $value) {
-		if (!array_key_exists($key, $jsonBefore)) {
-			$result[] = "  + {$key}: {$jsonAfter[$key]}";
-		}
-	}
-	$result[] = "}";
-	$result[] = "\n";
-	return implode("\n", $result);
+    $jsonBefore = json_decode(file_get_contents("$pathToFile1"), true);
+    $jsonAfter = json_decode(file_get_contents("$pathToFile2"), true);
+    $result = ["{"];
+    foreach ($jsonBefore as $key => $value) {
+        if (array_key_exists($key, $jsonAfter)) {
+            $result[] = $jsonAfter[$key] === $jsonBefore[$key] ? "    {$key}: {$jsonBefore[$key]}"
+             : "  - {$key}: {$jsonBefore[$key]}\n  + {$key}: {$jsonAfter[$key]}";
+        } else {
+            $result[] = "  - {$key}: {$jsonBefore[$key]}";
+        }
+    }
+    foreach ($jsonAfter as $key => $value) {
+        if (!array_key_exists($key, $jsonBefore)) {
+            $result[] = "  + {$key}: {$jsonAfter[$key]}";
+        }
+    }
+    $result[] = "}";
+    $result[] = "\n";
+    return implode("\n", $result);
 }
 
 // print_r(genDiff("before.json", "after.json"));
