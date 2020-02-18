04b93a9169782b1db689cf67ffe873a41cfe94f2
lev0607 <lev36rus@gmail.com>
2020-02-18 19:59:30 +0300

    add phpunit, phpcs

---
+++ tests/GenDiffTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Differ\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function Differ\genDiff\genDiff;
+
+class GenDiffTest extends TestCase
+{
+	public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
